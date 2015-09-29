e1dbe052b288c65ccb00b3af7191408b78409b5f
Minos Galanakis <minos197@gmail.com>
2015-09-29 11:01:51 +0100

    arp.py: Added threaded monitor implementation, data is now stored in dictionary format

--- arp.py
+++ arp.py
@@ -9,22 +9,41 @@ __email__   = "Minos Galanakis"
 __project__ = "codename"
 __date__    = "28-09-2015"
 
+import threading
 from scapy.all import *
-import socket
+from socket import gethostbyaddr 
+from subprocess import call, PIPE, Popen
 
 class ARPHandler():
 
   def __init__(self):
-    pass
-
+    self.net_table = {}
+  
   def arp_monitor_callback(self,pkt):
-    if ARP in pkt and pkt[ARP].op in (1,2):
-      return pkt.sprintf("%ARP.hwsrc% %ARP.psrc%")
+    """ Convert the return of the arp monitor to dictionary entries """
 
+    if ARP in pkt and pkt[ARP].op in (1,2):
+      hwaddr = pkt.sprintf(r"%ARP.hwsrc%")
+      ipsrc  = pkt.sprintf(r"%ARP.psrc%")
+      host   = self.arp_resolve(ipsrc)
+      self.net_table[ipsrc] = {'hostname':self.arp_resolve(ipsrc),'mac':hwaddr, 'alias':''}
+      print ("%s %s %s"%(host,ipsrc,hwaddr))
+  
   def arp_monitor(self):
-    sniff(prn=self.arp_monitor_callback, filter="arp", store=0)
+    """ Threaded monito proccess """
+
+    #Add background processes   
+    def background_arp_poll():
+      sniff(prn=self.arp_monitor_callback, filter="arp", store=0)
+
+    #covert to a thread and start it
+    thread = threading.Thread(target=background_arp_poll)
+    thread.daemon = True
+    thread.start() 
 
   def arp_scan(self):
+    """ Scan the network using ARP requests for alive hosts """
+
     conf.verb = 0
     ans,unans = srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst="192.168.0.1/24"),timeout = 2,iface = "eth0",inter=0.1)
 
@@ -32,12 +51,20 @@ class ARPHandler():
     for snd,rcv in ans:
       hwaddr = rcv.sprintf(r"%Ether.src%")
       ipsrc= rcv.sprintf(r"%ARP.psrc%")
-      hosts[ipsrc] = {'hostname':'','mac':hwaddr}
-      hosts[ipsrc]['hostname']=arp_resolve(ipsrc)
+      hosts[ipsrc] = {'hostname':'','mac':hwaddr, 'alias':''}
+      hosts[ipsrc]['hostname']=self.arp_resolve(ipsrc)
     return hosts
  
-  def arp_resolve(ip_target):
-    return socket.gethostbyaddr(ip_target)[0]
+  def arp_resolve(self, ip_target):
+    """ Resolve the hostname for given ip address """
+
+    return gethostbyaddr(ip_target)[0]
 
 if __name__ == "__main__":
-    pass
+    ah = ARPHandler()
+    #print ah.arp_scan()
+    ah.arp_monitor()
+    try:
+      while True: pass
+    except KeyboardInterrupt:
+      print ah.net_table
