face29311a448bc2d5fc8df63caf72ee930f949b
Minos Galanakis <minos197@gmail.com>
2015-10-02 09:45:49 +0100

    Added alertmanager, method to add entry to table and broadcast allerts

--- updater.py
+++ updater.py
@@ -18,6 +18,8 @@ from random import randint
 from copy import deepcopy
 from datetime import datetime, timedelta
 
+from allert import AllertManager
+
 # TODO remove it when testing is complete
 from test_dataset import get_data
 
@@ -29,11 +31,40 @@ class PageUpdater(Thread):
         self.socketio = socketio_srv
         self.arp_table = {}
         self.prune_period = prune_period
+        self.am = AllertManager()
         super(PageUpdater, self).__init__()
 
     def get_table(self):
-      return self.arp_table
-      
+        return self.arp_table
+    
+
+    def add_to_table(self, ip, entry):
+        """ Add a new entry to the ARP table and allert if it exists and is tracked """
+        
+        # If the device has the same IP as before
+        if ip in self.arp_table.keys() and entry['mac'] == self.arp_table[ip]['mac']: print 1
+        # If the ip has been modified but address still registered
+        elif entry['mac'] in repr(self.arp_table):
+          for key in self.arp_table:
+            if self.arp_table[key]['mac'] == entry['mac']: 
+               print 2
+               break
+            else: 
+              print 3
+              return False
+        # If it does not exist add it to the table
+        else:
+          self.arp_table[ip] = entry
+          print 4
+          return True
+        # Only broadcast allert if the device is tracked
+        color = self.arp_table[ip]['color']
+        if color:
+          al = self.arp_table[ip]['alias']
+          hst = self.arp_table[ip]['hostname']
+          self.am.broadcast("%s AKA %s has joined the network"%(hst,al),color)
+        return False
+
     def fake_refresh(self):
 
         #Pick a random device from the fake dataset and remove it
@@ -41,8 +72,11 @@ class PageUpdater(Thread):
         key = data_d.keys()[idx]
         # Add the new entry in the ARP table (this is will be replaced
         # by arp callbacks
-        self.arp_table[key] = data_d[key]
-        self.arp_table[key]["time"] = datetime.now()
+        #self.arp_table[key] = data_d[key]
+        #self.arp_table[key]["time"] = datetime.now()
+        tempentry = data_d[key]
+        tempentry["time"] = datetime.now()
+        self.add_to_table(key,tempentry)
 
     def clear_color(self, color):
         for ipkey in self.arp_table.keys():
